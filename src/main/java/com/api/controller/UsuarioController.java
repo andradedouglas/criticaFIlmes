764eb5fbe49350aa38704cabc059386cd1af6840
menesesdoug <31628610+menesesdoug@users.noreply.github.com>
2022-07-01 21:58:21 -0300

    Swagger att

--- src/main/java/com/api/controller/UsuarioController.java
+++ src/main/java/com/api/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import com.api.model.Perfil;
 import com.api.model.Usuario;
 import com.api.repository.UsuarioRepository;
 import com.api.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -32,12 +33,15 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
 
+    @Operation(summary = "Realiza o Login")
     @PostMapping("/login")
     public ResponseEntity<?> login (@Valid @RequestBody LoginForm loginForm) {
         Usuario u = usuarioRepository.findByEmailAndSenha(loginForm.getEmail(), loginForm.getSenha());
         if(u != null) return ResponseEntity.ok(new UsuarioDTO(u));
         else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
+
+    @Operation(summary = "Lista os usuários cadastrados")
     @GetMapping
     public ResponseEntity<?> listaUsuarios () {
         List<Usuario> listaUsuarios = usuarioRepository.findAll();
@@ -45,18 +49,21 @@ public class UsuarioController {
         else return new ResponseEntity<>("Nenhum usuário encontrado",HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Exibe um usuário a partir do ID")
     @GetMapping("/{id}")
     public ResponseEntity<?> exibeUsuarioPeloId (@PathVariable Long id) throws IllegalArgumentException{
         Usuario u = usuarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Não foi encontrado usuário com id = " + id));
         return new ResponseEntity<>(new UsuarioDTO(u), HttpStatus.OK);
     }
 
+    @Operation(summary = "Exibe um usuário a partir do Email")
     @GetMapping("/email/{email}")
     public ResponseEntity<?> exibeUsuarioPeloEmail (@PathVariable String email){
         Usuario u = usuarioRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Não foi encontrado usuário com email = " + email));
         return new ResponseEntity<>(new UsuarioDTO(u), HttpStatus.OK);
     }
 
+    @Operation(summary = "Cadastra um novo usuário")
     @PostMapping
     public ResponseEntity<?> cadastraUsuario (@Valid @RequestBody UsuarioForm usuarioForm, UriComponentsBuilder uriBuilder) throws IllegalArgumentException{
         if(!usuarioRepository.existsByEmail(usuarioForm.getEmail())){
@@ -66,6 +73,7 @@ public class UsuarioController {
         }else return new ResponseEntity<>("Usuário já existe",HttpStatus.BAD_REQUEST);
     }
 
+    @Operation(summary = "Atualiza o cadastro de um usuário")
     @PutMapping("/{id}")
     @Transactional
     public ResponseEntity<?> atualizaCadastroUsuario(@PathVariable Long id, @Valid @RequestBody UsuarioForm usuarioForm) throws IllegalArgumentException{
@@ -73,6 +81,7 @@ public class UsuarioController {
         return ResponseEntity.ok(new UsuarioDTO(usuarioForm.atualizar(id, usuarioRepository)));
     }
 
+    @Operation(summary = "Deleta um usuário")
     @DeleteMapping("{id}")
     public ResponseEntity<?> deletaUsuario(@PathVariable Long id) throws IllegalArgumentException{
         Usuario u = usuarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Não foi encontrado usuário com id = " + id));
@@ -80,7 +89,7 @@ public class UsuarioController {
         return new ResponseEntity<>("Deletado com sucesso", HttpStatus.OK);
     }
 
-    // ID USER VIA SESSAO
+    @Operation(summary = "Um moderador torna outro usuário também moderador")
     @PutMapping("/{idUsuarioModerador}/tornarModerador/{idUsuarioNaoModerador}")
     @Transactional
     public ResponseEntity<?> tornarModerador (@PathVariable Long idUsuarioModerador, @PathVariable Long idUsuarioNaoModerador){
